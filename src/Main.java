8e930b6783d2d046cb3106f59ebcee573b0cfb21
LucasCurry <lucas.curry@brightstraining.com>
2023-01-23 13:49:00 +0100

    Slänger in en till liten rackare

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Added this");
+        System.out.println("Slänger in en till liten rackare");
     }
 }
