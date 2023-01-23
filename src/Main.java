a1f7c8ac1421c172fbf08214234e0b280af489f5
LucasCurry <lucas.curry@brightstraining.com>
2023-01-23 16:09:12 +0100

    Time for merger!

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
         System.out.println("Added this");
         System.out.println("Slänger in en till liten rackare");
         System.out.println("För variant 2");
+        System.out.println("Nu ska vi merga dessa två projekt, spännande!");
     }
 }
